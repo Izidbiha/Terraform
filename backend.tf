b8530080f1c01fb886901e19c29c9c68384df08e
Izidbiha <64665342+Izidbiha@users.noreply.github.com>
2024-03-04 09:45:55 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,1 @@
-# Define Terraform backend using a S3 bucket for storing the Terraform state
-terraform {
-  backend "s3" {
-    bucket = "my-first-bucket-terraform-state"
-    key = "terraform-state/terraform.tfstate"
-    region = "eu-west-3"
- }
-}
+
