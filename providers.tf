bdedff2b6dd705b136a87d618249c27949e4be69
Izidbiha <64665342+Izidbiha@users.noreply.github.com>
2024-03-04 09:20:08 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -4,9 +4,17 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 2.13"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.3"
+    }
   }
   required_version = " 1.7.4"
 }
 provider "aws" {
   region = "eu-west-3"
+}
+
+provider "kubernetes" {
+
 }
