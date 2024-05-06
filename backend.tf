145bce303942943be3f09709d1f38a0401e64c4a
Izidbiha <64665342+Izidbiha@users.noreply.github.com>
2024-05-06 13:20:06 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,5 +4,7 @@ terraform {
     bucket = "my-first-bucket-terraform-state"
     key = "terraform-state/terraform.tfstate"   #use the bucket you created in your main.tf file
     region = "eu-west-3"
+    dynamodb_table = "terraform-sm-locks"
+    encrypt = true
  }
 }
